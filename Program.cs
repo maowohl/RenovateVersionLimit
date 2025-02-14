8da181188131766b28a129d4556c0c53469a1af3
Matheus <matheus.wohl@gmail.com>
2025-02-13 23:44:34 -0300

    refactor: get sources from nuget configuration
    also should fix a loop with dependency resolution

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
 using NuGet.Versioning;
 using Maowohl.RenovateVersionLimit.Classes;
+using NuGet.Configuration;
 
 namespace Maowohl.RenovateVersionLimit;
 
@@ -76,8 +77,16 @@ internal class Program
         CancellationToken cancellationToken = CancellationToken.None;
 
         SourceCacheContext cache = new SourceCacheContext();
-        SourceRepository repository = Repository.Factory.GetCoreV3("https://api.nuget.org/v3/index.json");
-        DependencyInfoResource dependencyInfoResource = await repository.GetResourceAsync<DependencyInfoResource>();
+
+        ISettings Settings = NuGet.Configuration.Settings.LoadDefaultSettings(
+                        Directory.GetCurrentDirectory(),
+                        configFileName: null,
+                        machineWideSettings: null);
+
+        IPackageSourceProvider SourceProvider = new PackageSourceProvider(Settings);
+        List<PackageSource> packageSources = SourceProvider.LoadPackageSources()
+                .Where(p => p.IsEnabled)
+                .ToList();
 
         async Task<List<Dependency>> WalkDependenciesAsync(int packageLevel, List<Dependency> packages, int maxDepth = 0)
         {
@@ -93,16 +102,36 @@ internal class Program
                 if (package.Level != packageLevel)
                     continue;
 
-                var result = await dependencyInfoResource.ResolvePackage(
-                    new PackageIdentity(package.Name, NuGetVersion.Parse(package.Version)),
-                    NuGetFramework.AnyFramework,
-                    cache,
-                    logger,
-                    cancellationToken
-                );
+                SourcePackageDependencyInfo? result = null;
+
+                foreach (PackageSource source in packageSources)
+                {
+                    if (result is not null)
+                        break; // found
+
+                    SourceRepository repository = Repository.Factory.GetCoreV3(source);
+                    DependencyInfoResource dependencyInfoResource = await repository.GetResourceAsync<DependencyInfoResource>();
+
+                    if (dependencyInfoResource is null)
+                        continue;
+
+                    result = await dependencyInfoResource.ResolvePackage(
+                        new PackageIdentity(package.Name, NuGetVersion.Parse(package.Version)),
+                        NuGetFramework.AnyFramework,
+                        cache,
+                        logger,
+                        cancellationToken
+                    );
+                }
+
+                if (result is null)
+                    throw new Exception($"Could not find package {package.Name} in any of the sources");
 
                 foreach (var dependency in result.Dependencies)
                 {
+                    if (packages.Any(p => p.Name == dependency.Id && p.Version == dependency.VersionRange.MinVersion!.ToString()))
+                        continue; // skip to avoid loop
+
                     packages.Add(new Dependency
                     {
                         Name = dependency.Id,
@@ -120,8 +149,8 @@ internal class Program
         {
             // Prefer newer versions by sorting first using semantic versioning
             var sortedPackages = packages.OrderBy(p => p.Name)
-                                         .ThenByDescending(p => SemanticVersion.Parse(p.Version), VersionComparer.Get(VersionComparison.Version))
-                                         .ToList();
+                .ThenByDescending(p => SemanticVersion.Parse(p.Version), VersionComparer.Get(VersionComparison.Version))
+                .ToList();
 
             // Use LINQ to deduplicate while preserving order
             var result = new List<Dependency>();
